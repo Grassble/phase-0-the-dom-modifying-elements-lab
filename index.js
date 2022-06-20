acebb91952bc7f158d315566a26e37af72174cbb
Peyton Harrison <peyton006@hotmail.com>
2022-06-20 11:22:43 -0500

    commit tree 95bd8df63854a6c66c2eac49c9ccd702ea70a0a9

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.querySelector('#main')
+console.log(document.querySelector('#main'))
+main.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "Peyton is the champion"
+main.appendChild(victory)
